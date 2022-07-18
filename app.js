6911b0ae2427b44deab839b26c2f237f00fd40f2
DESKTOP-LP8SBD0\DUVAN <duvanale2016@gmail.com>
2022-07-18 10:32:00 -0500

    add handlebars

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
 import express from "express";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import hbs from 'hbs';
 
-const app = express();
-const port = 8080;
+//dir_name
 const __filename = fileURLToPath(
     import.meta.url);
 const __dirname = dirname(__filename);
 
+const app = express();
+const port = 8080;
+//handlebars
+app.set('View engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+//definicion de ruta publica
 app.use(express.static('public'));
+
+const datos = {
+    titulo: 'curso node',
+    nombre: 'duan'
+};
+
+//definicion de rutas
+app.get('/', function(req, res) {
+    res.render('home.hbs', datos);
+});
 
 app.get('/hola-mundo', function(req, res) {
     res.send('Hello World')
 });
 
 app.get('/generic', function(req, res) {
-    res.sendFile(__dirname + '/public/generic.html');
+    res.render('generic.hbs', datos);
 });
 
 app.get('/elements', function(req, res) {
-    res.sendFile(__dirname + '/public/elements.html');
+    res.render('elements.hbs', datos);
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/404.html');
+    res.render('404.hbs');
 });
+
 app.listen(port)
